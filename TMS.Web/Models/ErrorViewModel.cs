ec98c6f9ea4daeffac462df0cd9621a7d7a20067
Александр Ягупьев <siaghupiev@mail.ru>
2023-12-05 11:27:07 +0500

    Add new TMS.Web blazor server

--- TMS.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TMS.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
